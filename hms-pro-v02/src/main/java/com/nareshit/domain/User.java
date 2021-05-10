45639c71d76e1a9a38f7152efb0b60f9541ce1dd
sudeepgit308143 <sudeep.connect19@gmail.com>
2021-05-10 19:45:42 +0530

    modified User.java file

--- hms-pro-v02/src/main/java/com/nareshit/domain/User.java
+++ hms-pro-v02/src/main/java/com/nareshit/domain/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 @Entity
-@Table(name="user_hms")
+@Table(name="user_hmspro")
 public class User implements Serializable{
 	/**
 	 * 
